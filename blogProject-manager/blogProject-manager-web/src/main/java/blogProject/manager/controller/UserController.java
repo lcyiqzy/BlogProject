a1b829f1674a04d736b63b9482fb133c5205fe83
zYong <shinnyong@163.com>
2017-08-10 09:44:15 +0800

    新后台

---
+++ blogProject-manager/blogProject-manager-web/src/main/java/blogProject/manager/controller/UserController.java
@@ -0,0 +1,14 @@
+package blogProject.manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usermanager")
+public class UserController {
+
+    @RequestMapping("/users")
+    public String userList() {
+        return "usermanager/user";
+    }
+}
