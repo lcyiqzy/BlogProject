fc171e1aeea267d8603a70d503636d3ac90c0c9f
zYong <shinnyong@163.com>
2017-08-10 11:35:36 +0800

    更新数据库

---
+++ blogProject-manager/blogProject-manager-pojo/src/main/java/blogProject/manager/bean/TTypePermission.java
@@ -0,0 +1,23 @@
+package blogProject.manager.bean;
+
+public class TTypePermission {
+    private Integer type;
+
+    private Integer permission;
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Integer permission) {
+        this.permission = permission;
+    }
+}
