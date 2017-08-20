3010436d36d72d2cb616d936d2e04d64f757a04e
zYong <shinnyong@163.com>
2017-08-20 17:41:21 +0800

    小修

--- blogProject-portal/src/main/java/blogProject/portal/controller/DispatchController.java
+++ blogProject-portal/src/main/java/blogProject/portal/controller/DispatchController.java
@@ -1,11 +1,18 @@
 package blogProject.portal.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -36,9 +43,16 @@ public class DispatchController {
         return "blog";
     }
     
+    @ResponseBody
     @RequestMapping("/backstage")
-    public String toBackstage() {
+    public void toBackstage(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
         String url = "http://127.0.0.1:8080/blogProject-manager-web/usermanager/manager";
-        return "redirect:" + url;
+//        request.getRequestDispatcher("blog").forward(request, response);
+        Cookie[] cookies = request.getCookies();
+        Cookie cookie = cookies[0];
+        System.out.println(cookie);
+        System.out.println(cookie.getName()+"="+cookie.getValue());
+        response.setHeader("Cookie", cookie.getName()+"="+cookie.getValue());
+        response.sendRedirect(url);
     }
 }
